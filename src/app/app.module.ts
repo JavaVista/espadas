fde424e7707d57bf2481f9bda372fba5c76387de
Javi Carrion <code-vista@outlook.com>
2022-09-09 22:40:08 -0400

    feat(player): add player component and player interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
