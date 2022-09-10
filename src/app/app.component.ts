9405522079c2a20916fb34dd54bb685ecff356be
Javi Carrion <code-vista@outlook.com>
2022-09-09 23:50:49 -0400

    feat(app): add create deck and shuffle deck functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card, cardSuits, cardValues } from './card/card';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  readonly deck: Card[] = createDeck();
 
+
+}
+
+function createDeck(): Card[] {
+  const resultDeck = [];
+  for (const suit of cardSuits) {
+    for (const value of cardValues) {
+      resultDeck.push({ value, suit });
+    }
+  }
+  shuffleDeck(resultDeck)
+  return resultDeck;
+}
+
+function shuffleDeck(deck: Card[]): void {
+  for (let index = 0; index < deck.length; index++) {
+    const swapIndex = Math.floor(Math.random() * deck.length - index);
+    [deck[index], deck[swapIndex]] = [deck[swapIndex], deck[index]];
+  }
 }
--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,4 +1,7 @@
+export const cardSuits = ['♠️', '♣️', '♥️', '♦️'] as const;
+export const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] as const;
+
 export interface Card {
-    value: string;
-    suit: '♠️' | '♣️' | '♥️' | '♦️';
+    readonly value: string;
+    readonly suit: '♠️' | '♣️' | '♥️' | '♦️';
 }
